d2fabaafc265bb30cca601f79a7b3c7b402decbc
rothbren <brenynfaith@byui.edu>
2020-10-08 09:54:07 -0600

    color updates

--- lesson4/js/javascript.js
+++ lesson4/js/javascript.js
@@ -1,3 +1,14 @@
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+const d = new Date();
+document.getElementById('currentdate').textContent=("The current month is " + monthNames[d.getMonth()]);
+/*
+document.getElementById('currentdate').textContent = (day + date + month + year); */
