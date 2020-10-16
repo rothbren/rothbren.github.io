495aa2333d855dc11ba2631badf7145969ec787b
rothbren <brenynfaith@byui.edu>
2020-10-16 09:41:03 -0600

    lesson 5 changes

---
+++ lesson5/js/javascript.js
@@ -0,0 +1,40 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+
+const daynames = [
+    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+];
+
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+
+document.getElementById('currentdate').textContent = fulldate;
+
+function pancakeday() {
+    var pancake;
+    if (daynames= "Friday"){
+        pancake = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion"
+    }
+    document.getElementById("pancake").innerHTML=pancake;
+}
