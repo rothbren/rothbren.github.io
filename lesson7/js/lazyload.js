5a2fbb8956a507a46a3e70b9ca6845958e27cebf
rothbren <brenynfaith@byui.edu>
2020-10-29 19:47:55 -0600

    trying to fix lazy loader

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,33 +1,30 @@
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
-  threshold: 0
+    threshold: 1
 };
 
 const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {
-    image.removeAttribute("data-src");
-  };
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
 };
 
-  if('IntersectionObserver' in window) {
+if ('IntersectionObserver' in window) {
     const imgObserver = new IntersectionObserver((items, imgObserver) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          imgObserver.unobserve(item.target);
-        }
-      });
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
     }, imgOptions);
-
-  imagesToLoad.forEach((img) => {
-      imgObserver.observe(img);
-    });
-  } else {
     imagesToLoad.forEach((img) => {
-      loadImages(img);
+        imgObserver.observe(img);
     });
-  } 
-
-  
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
