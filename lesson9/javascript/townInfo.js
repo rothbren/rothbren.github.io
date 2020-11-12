24cc8ede7c1b0c3b29d8279a9fc50a6f5cd986f1
rothbren <brenynfaith@byui.edu>
2020-11-12 10:19:48 -0700

    fixing the image and adding to lesson 9

--- lesson9/javascript/townInfo.js
+++ lesson9/javascript/townInfo.js
@@ -5,39 +5,38 @@ fetch(townURL)
         return response.json();
     })
     .then(function (jasonObject){
-        console.table(jasonObject); //checking the table, delete for final results
+        //console.table(jasonObject); checking the table, delete for final results
 
         const towns = jasonObject["towns"];
 
         for (let i = 0; i < towns.length; i++ ){
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let image = document.createElement('img');
+            let town_info = document.createElement('info_blocks');
+            let name = document.createElement('h2');
+            let photo = document.createElement('img');
             let motto = document.createElement('motto');
             let yearFounded = document.createElement('p');
-            let currentPop = document.createElement('p');
-            let averageRain = document.createElement('p');
-            let events = document.createElement('img');
+            let currentPopulation = document.createElement('p');
+            let averageRainfall = document.createElement('p');
+            //let events = document.createElement('p');
 
-            h2.textContent = towns[i].name;
+            name.textContent = towns[i].name;
             motto.textContent = towns[i].motto;
             yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            currentPop.textContent = 'Current Population: ' + towns[i].currentPop;
-            averageRain.textContent = 'Average Rainfall: ' + towns[i].averageRain;
-            events.textContent = 'Events: ' + towns[i].events;
-            image.setAttribute("src", towns[i].imageurl);
-            image.setAttribute("alt", h2.textContent + "-" + towns[i].order);
-            image.setAttribute("height", "205");
+            currentPopulation.textContent = 'Current Population: ' + towns[i].currentPopulation;
+            averageRainfall.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+            //events.textContent = 'Events: ' + towns[i].events;
+            photo.setAttribute("src", towns[i].photo);
+            photo.setAttribute("height", "205");
 
-            card.appendChild(h2);
-            card.appendChild(motto);
-            card.appendChild(yearFounded);
-            card.appendChild(currentPop);
-            card.appendChild(averageRain);
-            card.appendChild(events);
-            card.appendChild(image);
+            town_info.appendChild(name);
+            town_info.appendChild(motto);
+            town_info.appendChild(yearFounded);
+            town_info.appendChild(currentPopulation);
+            town_info.appendChild(averageRainfall);
+            //town_info.appendChild(events);
+            town_info.appendChild(photo);
 
-            document.querySelector('div.town_info').appendChild(card);
+            document.querySelector('div.town_info').appendChild(town_info);
 
         }
     })
