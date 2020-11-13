4026f51f2af99187b3566b322f7747d4f45e664d
rothbren <brenynfaith@byui.edu>
2020-11-12 18:21:48 -0700

    fixing js

--- lesson9/js/townInfo.js
+++ lesson9/js/townInfo.js
@@ -11,33 +11,33 @@ fetch(townURL)
 
         for (let i = 0; i < towns.length; i++ ){
             if(towns[i].name == 'Preston' || towns[i].name == 'Soda Springs'|| towns[i].name == 'Fish Haven'){
-            let town_info = document.createElement('info_blocks');
-            let name = document.createElement('h2');
-            let photo = document.createElement('img');
-            let motto = document.createElement('motto');
-            let yearFounded = document.createElement('p');
-            let currentPopulation = document.createElement('p');
-            let averageRainfall = document.createElement('p');
-            //let events = document.createElement('p');
+                let town_info = document.createElement('info_blocks');
+                let name = document.createElement('h2');
+                let photo = document.createElement('img');
+                let motto = document.createElement('motto');
+                let yearFounded = document.createElement('p');
+                let currentPopulation = document.createElement('p');
+                let averageRainfall = document.createElement('p');
+                //let events = document.createElement('p');
 
-            name.textContent = towns[i].name;
-            motto.textContent = towns[i].motto;
-            yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            currentPopulation.textContent = 'Current Population: ' + towns[i].currentPopulation;
-            averageRainfall.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
-            //events.textContent = 'Events: ' + towns[i].events;
-            photo.setAttribute("src", towns[i].photo);
-            photo.setAttribute("height", "205");
+                name.textContent = towns[i].name;
+                motto.textContent = towns[i].motto;
+                yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+                currentPopulation.textContent = 'Current Population: ' + towns[i].currentPopulation;
+                averageRainfall.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+                //events.textContent = 'Events: ' + towns[i].events;
+                photo.setAttribute("src", towns[i].photo);
+                photo.setAttribute("height", "205");
 
-            town_info.appendChild(name);
-            town_info.appendChild(motto);
-            town_info.appendChild(yearFounded);
-            town_info.appendChild(currentPopulation);
-            town_info.appendChild(averageRainfall);
-            //town_info.appendChild(events);
-            town_info.appendChild(photo);
+                town_info.appendChild(name);
+                town_info.appendChild(motto);
+                town_info.appendChild(yearFounded);
+                town_info.appendChild(currentPopulation);
+                town_info.appendChild(averageRainfall);
+                //town_info.appendChild(events);
+                town_info.appendChild(photo);
 
-            document.querySelector('div.town_info').appendChild(town_info);
+                document.querySelector('div.town_info').appendChild(town_info);
             }
         }
     })
