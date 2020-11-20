948fd24f246ed45a14ddc4a7265ba1121734cc2a
rothbren <brenynfaith@byui.edu>
2020-11-20 13:29:46 -0700

    adding lesson 11 folder

---
+++ lesson11/js/townInfo.js
@@ -0,0 +1,48 @@
+const townURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(townURL) 
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jasonObject){
+        //console.table(jasonObject); checking the table, delete for final results
+
+        const towns = jasonObject["towns"];
+
+        for (let i = 0; i < towns.length; i++ ){
+            if(towns[i].name == 'Preston' || towns[i].name == 'Soda Springs'|| towns[i].name == 'Fish Haven'){
+                let town_info = document.createElement('info_blocks');
+                let name = document.createElement('h2');
+                let photo = document.createElement('img');
+                let motto = document.createElement('motto');
+                let yearFounded = document.createElement('p');
+                let currentPopulation = document.createElement('p');
+                let averageRainfall = document.createElement('p');
+                //let events = document.createElement('p');
+
+                name.textContent = towns[i].name;
+                motto.textContent = towns[i].motto;
+                yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+                currentPopulation.textContent = 'Current Population: ' + towns[i].currentPopulation;
+                averageRainfall.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+                //events.textContent = 'Events: ' + towns[i].events;
+                photo.setAttribute('src','images/'+ towns[i].photo);
+                photo.setAttribute("height", "205");
+                photo.setAttribute('alt', name.textContent + '-' + towns[i].order);
+
+                town_info.appendChild(name);
+                town_info.appendChild(motto);
+                town_info.appendChild(yearFounded);
+                town_info.appendChild(currentPopulation);
+                town_info.appendChild(averageRainfall);
+                //town_info.appendChild(events);
+                town_info.appendChild(photo);
+
+                document.querySelector('div.town_info').appendChild(town_info);
+            }
+        }
+    })
+
+    
+
+    
---
+++ lesson11/js/weatherapi.js
@@ -0,0 +1,39 @@
+const weatherURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=f9208b18eeb79b0482b30e396e20dd9e";
+
+fetch(weatherURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        //console.log(jsObject);
+
+        document.getElementById('currently').textContent = jsObject.weather[0].main;
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('wind_speed').textContent = jsObject.wind.speed;
+        document.getElementById('wind_chill').textContent = jsObject.weather.main;
+    });
+
+
+const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=f9208b18eeb79b0482b30e396e20dd9e";;
+fetch(apiURL)
+    .then(response => response.json())
+    .then(jsObject => {
+
+
+        const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const forecast = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
+
+        for (let day = 0; day < forecast.length; day++) {
+            const d = new Date(forecast[day].dt_txt);
+            document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
+            document.getElementById(`forecast${day+1}`).textContent = forecast[day].main.temp;
+            //console.log(forecast);
+
+           
+        }
+    });
+
+ const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
+            const desc = jsObject.weather[0].description; // note how we reference the weather array
+            document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
+            document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
+            document.getElementById('icon').setAttribute('alt', desc);
