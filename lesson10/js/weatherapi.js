1ac120951837e51dc3fa08f42416620031fe7d4d
rothbren <brenynfaith@byui.edu>
2020-11-20 13:56:38 -0700

    still trying to fux things in chapter 10

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const weatherURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=f9208b18eeb79b0482b30e396e20dd9e";
+const weatherURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=f9208b18eeb79b0482b30e396e20dd9e";
 
 fetch(weatherURL)
     .then((response) => response.json())
@@ -10,10 +10,17 @@ fetch(weatherURL)
         document.getElementById('humidity').textContent = jsObject.main.humidity;
         document.getElementById('wind_speed').textContent = jsObject.wind.speed;
         document.getElementById('wind_chill').textContent = jsObject.weather.main;
+        
+        
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; 
+            const desc = jsObject.weather[0].description; 
+            document.getElementById('imagesrc').textContent = imagesrc; 
+            document.getElementById('icon').setAttribute('src', imagesrc);
+            document.getElementById('icon').setAttribute('alt', desc);
     });
 
 
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=f9208b18eeb79b0482b30e396e20dd9e";;
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=f9208b18eeb79b0482b30e396e20dd9e";;
 fetch(apiURL)
     .then(response => response.json())
     .then(jsObject => {
@@ -26,14 +33,9 @@ fetch(apiURL)
             const d = new Date(forecast[day].dt_txt);
             document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
             document.getElementById(`forecast${day+1}`).textContent = forecast[day].main.temp;
-            //console.log(forecast);
-
-           
+            //console.log(forecast);   
         }
+        
     });
 
- const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
-            const desc = jsObject.weather[0].description; // note how we reference the weather array
-            document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
-            document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
-            document.getElementById('icon').setAttribute('alt', desc);
+ 
