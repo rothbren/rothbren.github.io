847d844d9132044a40b145c08eb4027fcd17f4b5
rothbren <brenynfaith@byui.edu>
2020-12-03 09:07:21 -0700

    updates

--- finalLesson/js/java.js
+++ finalLesson/js/java.js
@@ -31,3 +31,63 @@ const year = d.getFullYear();
 const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
 
 document.getElementById('currentdate').textContent = fulldate;
+
+/***********FORM************/
+
+var params = new URLSearchParams(location.search);
+var data = '<br/><strong>Your Reservation: </strong><br/>';
+data = data + 'Your Name: ' + params.get('fname') + '<br/>';
+data = data + 'Email: ' + params.get('email') + '<br/>';
+data = data + 'Phone: ' + params.get('phone') + '<br/>';
+data = data + 'Zipcode: ' + params.get('zipcode') + '<br/>';
+data = data + 'Home State: ' + params.get('state') + '<br/>';
+data = data + 'Cruise Line: ' + params.get('cruise') + '<br/>';
+data = data + 'Date of Adventure: ' + params.get('date') + '<br/>';
+data = data + 'Number in Party: ' + params.get('number') + '<br/>';
+data = data + 'Rental Type: ' + params.get('rentalType') + '<br/>';
+data = data + 'Special Instructions: ' + params.get('comments_box') + '<br/><br/>';
+data = data + 'Drop Off Instructions: ' + params.get('comments_box') + '<br/><br/>';
+data = data + 'Additional Information and Comments: ' + params.get('comments_box') + '<br/><br/>';
+document.querySelector(".reservationInfo").innerHTML = data;
+
+/************RENTALS***********/
+const rentalURL = "data/rental.json";
+
+fetch(rentalURL)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (jasonObject){
+        console.log(rental);
+
+        const rental = jasonObject["rentals"];
+
+        for(let i = 0; i<rental.length; i++){
+            let name = document.createElement('name');
+            let maxpersons= document.createElement('maxPersons');
+            let price= document.createElement('reservationprice');
+            let halfP= document.createElement('halfP');
+            let fullP= document.createElement('fullP');
+            let walkinprice= document.createElement('walkinprice');
+            let other= document.createElement('other');
+
+            name.textContent = rental[i].name;
+            maxpersons.textContent = "Max Number of People: " + rental[i].maxpersons;
+            price.textContent = "Price: " + rental[i].price;
+            halfP.textContent = "Half Day Price: "+ rental[i].halfP;
+            fullP.textContent = "Full Day Price: "+ rental[i].fullP;
+            walkinprice.textContent = "Price without Reservation: "+ rental[i].walkinprice;
+            other.textContent = rental[i].other;
+
+            rentalInfo.appendChild(name);
+            rentalInfo.appendChild(maxpersons);
+            rentalInfo.appendChild(price);
+            rentalInfo.appendChild(halfP);
+            rentalInfo.appendChild(fullP);
+            rentalInfo.appendChild(walkinprice);
+            rentalInfo.appendChild(other);
+
+            document.querySelector('div.rentalInfo').appendChild(rentalInfo);
+
+        }
+    })
--- finalLesson/js/weather.js
+++ finalLesson/js/weather.js
@@ -1,116 +1,46 @@
-var currentURL = window.location.href;
-let currentId = "5604473";
-if (currentURL.indexOf("preston.html") > 0) {
-    currentId = "5604473";
-} else if (currentId.indexOf("sodaSprings.html") > 0) {
-    currentId = "5607916";
-} else if (currentId.indexOf("fishHaven.html") > 0) {
-    currentId = "lat=42.0380399&lon=-111.4048681";
-}
+// /***LINKS TO THE WEATHER_TOWN INFORMATION JSON FILES***/
+// const weatherURL = "api.openweathermap.org/data/2.5/weather?id= l&appid=&units=imperial&appid=f9208b18eeb79b0482b30e396e20dd9e";
+// //const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + currentId + "&units=imperial&appid=f9208b18eeb79b0482b30e396e20dd9e";
 
-/***LINKS TO THE WEATHER_TOWN INFORMATION JSON FILES***/
-const weatherURL = "https://api.openweathermap.org/data/2.5/weather?id=" + currentId + "&units=imperial&appid=f9208b18eeb79b0482b30e396e20dd9e";
-const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + currentId + "&units=imperial&appid=f9208b18eeb79b0482b30e396e20dd9e";
-const townInfoURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+// fetch(weatherURL)
+//     .then((response) => response.json())
+//     .then((jsObject) => {
+//         //console.log(jsObject);
+//         document.getElementById('currently').innerHTML = jsObject.weather[0].description;
+//         document.getElementById('current-temp').textContent = jsObject.main.temp;
+//         document.getElementById('humidity').textContent = jsObject.main.humidity;
+//         document.getElementById('wind_speed').textContent = jsObject.wind.speed;
 
-fetch(weatherURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-        //console.log(jsObject);
-        document.getElementById('currently').innerHTML = jsObject.weather[0].description;
-        document.getElementById('current-temp').textContent = jsObject.main.temp;
-        document.getElementById('humidity').textContent = jsObject.main.humidity;
-        document.getElementById('wind_speed').textContent = jsObject.wind.speed;
+//         let t = parseFloat(jsObject.main.temp);
+//         let s = parseFloat(jsObject.wind.speed);
+//         let wind_chill = "N/A";
+//         if (t <= 50 && s >= 3) {
+//             let f = (35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275 * (t * (Math.pow(s, 0.16))));
+//             wind_chill = Math.round(f);
+//         };
+//         document.getElementById("wind_chill").innerHTML = wind_chill;
 
-        let t = parseFloat(jsObject.main.temp);
-        let s = parseFloat(jsObject.wind.speed);
-        let wind_chill = "N/A";
-        if (t <= 50 && s >= 3) {
-            let f = (35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275 * (t * (Math.pow(s, 0.16))));
-            wind_chill = Math.round(f);
-        };
-        document.getElementById("wind_chill").innerHTML = wind_chill;
+//     });
 
-    });
+// fetch(apiURL)
+//     .then(response => response.json())
+//     .then(jsObject => {
 
-fetch(apiURL)
-    .then(response => response.json())
-    .then(jsObject => {
+//         const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//         const forecast = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
 
-        const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        const forecast = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
+//         for (let day = 0; day < forecast.length; day++) {
+//             const d = new Date(forecast[day].dt_txt);
+//             document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
+//             document.getElementById(`forecast${day+1}`).textContent = forecast[day].main.temp;
+//             //console.log(forecast);
+//             const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
+//             const desc = forecast[day].weather[0].description;
+//             document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
+//             document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast[day].main.temp);
+//             document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+//             document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
 
-        for (let day = 0; day < forecast.length; day++) {
-            const d = new Date(forecast[day].dt_txt);
-            document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
-            document.getElementById(`forecast${day+1}`).textContent = forecast[day].main.temp;
-            //console.log(forecast);
-            const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
-            const desc = forecast[day].weather[0].description;
-            document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
-            document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast[day].main.temp);
-            document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
-            document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
+//         }
+//     });
 
-        }
-    });
-
-
-/*****CURRENT EVENTS******/
-fetch(townInfoURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        //console.table(jsonObject);
-
-
-        const towns = jsonObject['towns'];
-        for (let i = 0; i < towns.length; i++) {
-
-            const eventInfo = document.createElement('div');
-
-            if (towns[i].name == "Preston" && currentURL.indexOf("preston.html") > 0) {
-
-
-
-                eventInfo.setAttribute('class', 'eventInfo');
-                for (let x = 0; x <= towns[i].events.length; x++) {
-
-                    let para = document.createElement('p');
-
-                    para.textContent = towns[i].events[x];
-
-                    eventInfo.appendChild(para);
-
-                }
-
-            } else if (towns[i].name == "Soda Springs" && currentURL.indexOf('sodaSprings.html') > 0) {
-
-                eventInfo.setAttribute('class', 'eventInfo');
-                for (let x = 0; x <= towns[i].events.length; x++) {
-
-                    let para = document.createElement('p');
-
-                    para.textContent = towns[i].events[x];
-
-                    eventInfo.appendChild(para);
-                }
-            } else if (towns[i].name == "Fish Haven" && currentURL.indexOf('fishHaven.html') > 0) {
-
-                eventInfo.setAttribute('class', 'eventInfo');
-                for (let x = 0; x <= towns[i].events.length; x++) {
-
-                    let para = document.createElement('p');
-
-                    para.textContent = towns[i].events[x];
-
-                    eventInfo.appendChild(para);
-                }
-
-            }
-
-            document.getElementById('events').appendChild(eventInfo);
-        }
-
-    });
